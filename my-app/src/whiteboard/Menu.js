881a36d3db9448f7749dcd8731c0a92f4d0410c0
taesik mun <taesic12@knou.ac.kr>
2023-07-15 01:02:53 +0900

    tool button

--- my-app/src/whiteboard/Menu.js
+++ my-app/src/whiteboard/Menu.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import rectangleIcon from '../resources/icons/rectangle.svg'
+import {toolTypes} from "../constants";
+const IconButton = ({src,type}) =>{
+  return <button className={'menu_button'}>
+    <img src={src} alt="" width={'80%'} height={'80%'} />
+  </button>
+}
 
 const Menu = () => {
   return (
       <div className={'menu_container'}>
-        
+        <IconButton src={rectangleIcon} type={toolTypes.RECTANGLE}/>
       </div>
   );
 };
